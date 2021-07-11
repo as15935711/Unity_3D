67461a8f3ab72f9c7da9a43aacb3a96fb7305e74
as15935711 <82298466+as15935711@users.noreply.github.com>
2021-07-11 14:02:29 +0800

    天空

---
+++ Unity_3D/Assets/spripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region 欄位
+    private Rigidbody rig;
+    private Animator ani;
+    private Transform traCamera;
+    #endregion
+
+
+    #region 事件
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        ani = transform.Find("男生").GetComponent<Animator>();
+        traCamera = GameObject.Find("攝影機").transform;
+    }
+    #endregion
+
+
+    #region 方法
+
+    #endregion
+}
